07b4cc23f144d7fce7fcf0c82e3c1c5374c201f8
Pi <piisthebestthingintheuniverse@gmail.com>
2024-06-07 15:55:47 -0700

    cloudflare bypass: disable feature

--- src/tools/bypassCloudflare.js
+++ src/tools/bypassCloudflare.js
@@ -1,30 +1,34 @@
+const config = require("../../config.json");
+
 let _getPage = async (url) => {
-	return null;
+	return await (await fetch(url)).json();
 };
 
 async function getPage(url) {
 	return await _getPage(url);
 }
 
-(async () => {
-	const { connect } = await import("puppeteer-real-browser");
-	const { page, browser } = await connect({
-		turnstile: true,
-		headless: "auto",
-	});
-	await page.goto("https://apibeta.deeeep.io", {
-		waitUntil: "domcontentloaded",
-	});
-	_getPage = async (url) => {
-		return await page.evaluate(async (u) => {
-			try {
-				return await (await fetch(u)).json();
-			} catch {
-				return null;
-			}
-		}, url);
-	};
-})();
+if (config.bypassCloudflare) {
+	(async () => {
+		const { connect } = await import("puppeteer-real-browser");
+		const { page, browser } = await connect({
+			turnstile: true,
+			headless: "auto",
+		});
+		await page.goto("https://apibeta.deeeep.io", {
+			waitUntil: "domcontentloaded",
+		});
+		_getPage = async (url) => {
+			return await page.evaluate(async (u) => {
+				try {
+					return await (await fetch(u)).json();
+				} catch {
+					return null;
+				}
+			}, url);
+		};
+	})();
+}
 
 module.exports = {
 	getPage,
