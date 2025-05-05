81e052554beaf80259bcc8df6c50522e0d04126b
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-05-05 16:12:42 -0700

    fix bug

--- src/utils.js
+++ src/utils.js
@@ -44,115 +44,117 @@ async function userProfileEmbed(profileData, statsData, socialNetworksData) {
 		);
 	}
 
-	return {
-		embeds: [
-			new EmbedBuilder()
-				.setURL(`https://deeeep.io/u/${profileData.username}`)
-				.setTitle(
-					`${profileData.username} ${profileData.verified ? config.emojis.verified : ""}`,
-				)
-				.setDescription(profileData.about?.toString() || "*No description*")
-				.addFields(
-					profileData.badges.length > 0 && {
-						name: "Badges",
-						value: profileData.badges
-							.map((b) => config.emojis[b.slug])
-							.join(" "),
-						inline: false,
-					},
-					{
-						name: "Kills",
-						value: `${numberWithCommas(profileData.kill_count)} (#${numberWithCommas(statsData.rank_kc)})`,
-						inline: true,
-					},
-					{
-						name: "Play count",
-						value: `${numberWithCommas(profileData.play_count)} (#${numberWithCommas(statsData.rank_pc)})`,
-						inline: true,
-					},
-					{
-						name: "KDR",
-						value: `${
-							(profileData.play_count === 0
-								? profileData.kill_count
-								: profileData.kill_count / profileData.play_count
-							).toFixed(2) || "*N/A*"
-						}`,
-						inline: true,
-					},
-					{
-						name: "High score",
-						value: `${numberWithCommas(profileData.highest_score)} (#${numberWithCommas(statsData.rank_hs)})`,
-						inline: true,
-					},
-					{
-						name: "Pearl defense",
-						value:
-							statsData.pd === null
-								? "*No data*"
-								: `${numberWithCommas(statsData.pd.played)} played, ${numberWithCommas(statsData.pd.won)} won; win ratio: ${
-										statsData.pd.ratio
-									}%`,
-						inline: false,
-					},
-					{
-						name: "Coins",
-						value: numberWithCommas(profileData.coins),
-						inline: true,
-					},
-					{
-						name: "XP",
-						value: `${numberWithCommas(profileData.xp)} (Tier ${profileData.tier || 1})`,
-						inline: true,
-					},
-					{
-						name: "Profile views",
-						value: numberWithCommas(profileData.profile_views),
-						inline: true,
-					},
-					{
-						name: "Date created",
-						value: `<t:${Math.round(Date.parse(profileData.date_created) / 1000)}:F>`,
-						inline: true,
-					},
-					{
-						name: "Date last played",
-						value: `<t:${Math.round(Date.parse(profileData.date_last_played) / 1000)}:F>`,
-						inline: true,
-					},
-					{
-						name: "Death message",
-						value: profileData.description?.toString() || "*No death message*",
-						inline: false,
-					},
-					socialNetworksData.length > 0 && {
-						name: "Social networks",
-						value: socialNetworksData
-							.map((n) => {
-								if (typeof socialNetworks[n.platform_id] === "string") {
-									return `${config.emojis[n.platform_id]} [${n.platform_user_id}](${socialNetworks[n.platform_id] + n.platform_user_id})`;
-								}
-								if (typeof socialNetworks[n.platform_id] === "boolean") {
-									return `${config.emojis[n.platform_id]} [${n.platform_user_id}](${n.platform_user_url})`;
-								}
-								return `${config.emojis[n.platform_id]} ${n.platform_user_id}`;
-							})
-							.join("\n"),
-						inline: false,
-					},
-				)
-				.setFooter({
-					text: `ID: ${profileData.id}`,
+	const embed = new EmbedBuilder()
+		.setURL(`https://deeeep.io/u/${profileData.username}`)
+		.setTitle(
+			`${profileData.username} ${profileData.verified ? config.emojis.verified : ""}`,
+		)
+		.setDescription(profileData.about?.toString() || "*No description*")
+		.addFields(
+			{
+				name: "Kills",
+				value: `${numberWithCommas(profileData.kill_count)} (#${numberWithCommas(statsData.rank_kc)})`,
+				inline: true,
+			},
+			{
+				name: "Play count",
+				value: `${numberWithCommas(profileData.play_count)} (#${numberWithCommas(statsData.rank_pc)})`,
+				inline: true,
+			},
+			{
+				name: "KDR",
+				value: `${
+					(profileData.play_count === 0
+						? profileData.kill_count
+						: profileData.kill_count / profileData.play_count
+					).toFixed(2) || "*N/A*"
+				}`,
+				inline: true,
+			},
+			{
+				name: "High score",
+				value: `${numberWithCommas(profileData.highest_score)} (#${numberWithCommas(statsData.rank_hs)})`,
+				inline: true,
+			},
+			{
+				name: "Pearl defense",
+				value:
+					statsData.pd === null
+						? "*No data*"
+						: `${numberWithCommas(statsData.pd.played)} played, ${numberWithCommas(statsData.pd.won)} won; win ratio: ${
+								statsData.pd.ratio
+							}%`,
+				inline: false,
+			},
+			{
+				name: "Coins",
+				value: numberWithCommas(profileData.coins),
+				inline: true,
+			},
+			{
+				name: "XP",
+				value: `${numberWithCommas(profileData.xp)} (Tier ${profileData.tier || 1})`,
+				inline: true,
+			},
+			{
+				name: "Profile views",
+				value: numberWithCommas(profileData.profile_views),
+				inline: true,
+			},
+			{
+				name: "Date created",
+				value: `<t:${Math.round(Date.parse(profileData.date_created) / 1000)}:F>`,
+				inline: true,
+			},
+			{
+				name: "Date last played",
+				value: `<t:${Math.round(Date.parse(profileData.date_last_played) / 1000)}:F>`,
+				inline: true,
+			},
+			{
+				name: "Death message",
+				value: profileData.description?.toString() || "*No death message*",
+				inline: false,
+			},
+		)
+		.setFooter({
+			text: `ID: ${profileData.id}`,
+		})
+		.setImage(
+			profileData.picture
+				? isApng
+					? "attachment://pfp.gif"
+					: `https://cdn.deeeep.io/uploads/avatars/${profileData.picture}`
+				: "https://deeeep.io/img/avatar.png",
+		)
+		.setTimestamp();
+
+	profileData.badges.length > 0 &&
+		embed.addFields({
+			name: "Badges",
+			value: profileData.badges.map((b) => config.emojis[b.slug]).join(" "),
+			inline: false,
+		});
+
+	socialNetworksData.length > 0 &&
+		embed.addFields({
+			name: "Social networks",
+			value: socialNetworksData
+				.map((n) => {
+					if (typeof socialNetworks[n.platform_id] === "string") {
+						return `${config.emojis[n.platform_id]} [${n.platform_user_id}](${socialNetworks[n.platform_id] + n.platform_user_id})`;
+					}
+					if (typeof socialNetworks[n.platform_id] === "boolean") {
+						return `${config.emojis[n.platform_id]} [${n.platform_user_id}](${n.platform_user_url})`;
+					}
+					return `${config.emojis[n.platform_id]} ${n.platform_user_id}`;
 				})
-				.setImage(
-					profileData.picture
-						? isApng
-							? "attachment://pfp.gif"
-							: `https://cdn.deeeep.io/uploads/avatars/${profileData.picture}`
-						: "https://deeeep.io/img/avatar.png",
-				)
-				.setTimestamp(),
-		],
+				.join("\n"),
+			inline: false,
+		});
+
+	return {
+		embeds: [embed],
 		files: attachments,
 	};
 }
