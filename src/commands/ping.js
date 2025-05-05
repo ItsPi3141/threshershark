241df658d8798c7ea7559216905e1b065cdaf662
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-05-05 13:15:40 -0700

    improve ping calculation

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -10,15 +10,23 @@ module.exports = {
 		/** @type {import("discord.js").Interaction} */ interaction,
 		/** @type {import("discord.js").Client} */ client,
 	) {
-		const t1 = Date.now();
+		const t1_discord = Date.now();
+		await interaction.reply(`${config.emojis.loading} Calculating ping...`);
+		const t2_discord = Date.now();
+
+		const t1_api = Date.now();
+		await getPage("https://api.deeeep.io/pets");
+		const t2_api = Date.now();
+		const t1_db = Date.now();
 		await getPage("https://api.deeeep.io/users/5");
-		const t2 = Date.now();
-		interaction
-			.reply(`${config.emojis.loading} Calculating ping...`)
-			.then((i) => {
-				i.edit(
-					`Discord: \`${Math.round(Date.now() - t1)}ms\`\nDeeeep.io: \`${Math.round(t2 - t1)}ms\``,
-				);
-			});
+		const t2_db = Date.now();
+
+		interaction.editReply(
+			[
+				`Discord: \`${Math.round(t2_discord - t1_discord)}ms\``,
+				`Deeeep.io (static data): \`${Math.round(t2_api - t1_api)}ms\``,
+				`Deeeep.io (database query): \`${Math.round(t2_db - t1_db)}ms\``,
+			].join("\n"),
+		);
 	},
 };
