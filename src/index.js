1bed59ba34750f6d77957ea845ff9827fe249774
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2026-05-05 03:29:59 -0700

    disable error logging for multiple resolves

--- src/index.js
+++ src/index.js
@@ -135,8 +135,8 @@ process.on("uncaughtExceptionMonitor", (err, origin) => {
 	console.log(err, origin);
 });
 process.on("multipleResolves", (type, promise, reason) => {
-	console.log(" [Error_Handling] :: Multiple Resolves");
-	console.log(type, promise, reason);
+	// console.log(" [Error_Handling] :: Multiple Resolves");
+	// console.log(type, promise, reason);
 });
 
 const exitHandler = (code) => {
