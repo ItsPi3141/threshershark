71452b2bd9e419e3d32cf407ee056f8f5e55c5f1
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2026-05-05 03:06:50 -0700

    error log

--- src/index.js
+++ src/index.js
@@ -123,20 +123,20 @@ client.login(process.env.TOKEN);
 
 // please do not crash my bot
 process.on("unhandledRejection", (reason, p) => {
-	// console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
-	// console.log(reason, p);
+	console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+	console.log(reason, p);
 });
 process.on("uncaughtException", (err, origin) => {
-	// console.log(" [Error_Handling] :: Uncaught Exception/Catch");
-	// console.log(err, origin);
+	console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+	console.log(err, origin);
 });
 process.on("uncaughtExceptionMonitor", (err, origin) => {
-	// console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
-	// console.log(err, origin);
+	console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+	console.log(err, origin);
 });
 process.on("multipleResolves", (type, promise, reason) => {
-	// console.log(" [Error_Handling] :: Multiple Resolves");
-	// console.log(type, promise, reason);
+	console.log(" [Error_Handling] :: Multiple Resolves");
+	console.log(type, promise, reason);
 });
 
 process.stdin.resume();
